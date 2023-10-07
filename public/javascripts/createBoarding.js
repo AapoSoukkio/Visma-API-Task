2f4cb00cfd67f8e1d4f92073c07d677916c38329
AapoSoukkio <aapo.soukkio@edu.savonia.fi>
2023-10-07 16:45:16 +0300

    Page for creating new boarding and logic for sending POST request to the server

---
+++ public/javascripts/createBoarding.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("boarding-form"); 
+  
+    form.addEventListener("submit", async function (event) {
+      event.preventDefault(); 
+  
+      const vatNumber = document.getElementById("vatNumber").value;
+      const pricePackage = document.getElementById("pricePackage").value;
+  
+      try {
+        // POST request to the server
+        const response = await fetch("/create-boarding", { 
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ vatNumber, pricePackage }),
+        });
+  
+        if (response.ok) {
+          const result = await response.json();
+          document.getElementById("result-message").textContent = result.message;
+        } else {
+          document.getElementById("result-message").textContent = "Boarding creation failed: " + response.statusText;
+        }
+      } catch (error) {
+        // Handle network errors by showing an error message
+        console.error(error);
+        document.getElementById("result-message").textContent = "Network error";
+      }
+    });
+});
