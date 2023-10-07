741c67ca3aa4031f2535ed5d3943b16f540b7744
AapoSoukkio <aapo.soukkio@edu.savonia.fi>
2023-10-07 19:01:54 +0300

    added hardcoded values for boarding and alert to show response

--- public/javascripts/createBoarding.js
+++ public/javascripts/createBoarding.js
@@ -1,32 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("boarding-form"); 
-  
+
     form.addEventListener("submit", async function (event) {
-      event.preventDefault(); 
-  
-      const vatNumber = document.getElementById("vatNumber").value;
-      const pricePackage = document.getElementById("pricePackage").value;
-  
-      try {
-        // POST request to the server
-        const response = await fetch('/create-boarding', { 
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ vatNumber, pricePackage }),
-        });
-  
-        if (response.ok) {
-          const result = await response.json();
-          document.getElementById("result-message").textContent = result.message;
-        } else {
-          document.getElementById("result-message").textContent = "Boarding creation failed: " + response.statusText;
+        event.preventDefault(); 
+
+        const vatNumber = document.getElementById("vatNumber").value;
+        const pricePackage = document.getElementById("pricePackage").value;
+
+        try {
+            // POST request to the server
+            const response = await fetch('/create-boarding', { 
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ vatNumber, pricePackage }),
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                alert(result.message); // Show a simple alert
+            } else {
+                alert("Boarding creation failed: " + response.statusText); // Show an alert for the error
+            }
+        } catch (error) {
+            // Handle network errors by showing an error message
+            console.error(error);
+            alert("Network error");
         }
-      } catch (error) {
-        // Handle network errors by showing an error message
-        console.error(error);
-        document.getElementById("result-message").textContent = "Network error";
-      }
     });
 });
